cf90721997ccd369ccdbaafc32d6c991555b211c
Aisack23 <cesar.isaac.tovar22@gmail.com>
2026-06-23 14:01:12 -0600

    The common directory was removed and its files moved to mapper

--- src/main/java/com/example/OhBike/mapper/UserMapper.java
+++ src/main/java/com/example/OhBike/mapper/UserMapper.java
@@ -1,4 +1,4 @@
-package com.example.OhBike.common.mapper;
+package com.example.OhBike.mapper;
 
 import com.example.OhBike.dto.request.RegisterRequest;
 import com.example.OhBike.dto.response.UserResponse;
